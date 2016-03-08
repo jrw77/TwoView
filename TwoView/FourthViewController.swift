64cb275e5e4626f00503d718658e638139ef0c43
weimar <weimar@maca610905weimar.local>
2016-03-08 12:38:39 -0600

    Added Fourth view controller.

--- TwoView/FourthViewController.swift
+++ TwoView/FourthViewController.swift
@@ -1,30 +1,24 @@
 //
-//  ThirdViewController.swift
+//  çViewConrtoller.swift
 //  TwoView
 //
-//  Created by weimar on 2/15/16.
+//  Created by weimar on 3/8/16.
 //  Copyright © 2016 Weimar. All rights reserved.
 //
 
 import UIKit
 
-class ThirdViewController: LabelledViewController {
-
+class FourthViewController: LabelledViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let destination = segue.destinationViewController as! LabelledViewController
-        destination.labelText = "Coming from Third view\n"
+        destination.labelText = "Coming from fourth view\n"
     }
-
+    
 }
